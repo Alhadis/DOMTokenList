0b05cc93b2c179e98f24fd62f3407e4865672641
Alhadis <gardnerjohng@gmail.com>
2016-02-03 07:06:58 +1100

    Improve compressibility of SVG-specific fixes

--- svg-fix.js
+++ svg-fix.js
@@ -6,35 +6,47 @@
 (function(){
 	"use strict";
 	
-	var W3      = "http://www.w3.org/";
-	var NS_SVG  = W3 + "2000/svg";
-	var NS_HTML = W3 + "1999/xhtml";
-	var CORKED  = "__CORKED";
-	var SVG     = window.SVGElement;
+	var W3                 = "http://www.w3.org/";
+	var NS_SVG             = W3 + "2000/svg";
+	var NS_HTML            = W3 + "1999/xhtml";
+	var CORKED             = "__CORKED";
+	var CREATE_ELEMENT     = "createElement";
+	var CREATE_ELEMENT_NS  = CREATE_ELEMENT + "NS";
+	var DIV                = "div";
+	var CLASS_             = "class";
+	var CLASS_LIST         = CLASS_ + "List";
+	var SET_ATTR           = "setAttribute";
+	
+	var WIN                = window;
+	var DOC                = document;
+	var SVG                = WIN.SVGElement;
 
+	
+	var mirror, reflections;
+	var div, createElement;
+	
 	/** No classList support in SVG elements */
-	if(SVG && !("classList" in document.createElementNS(NS_SVG, "g"))){
+	if(SVG && !(CLASS_LIST in DOC[CREATE_ELEMENT_NS](NS_SVG, "g"))){
 		
 		/** Stop IE10-11 choking on token-list.js when running it from inside an SVG document */
-		var div = document.createElement("div");
+		div = DOC[CREATE_ELEMENT](DIV);
 		if(null === div.namespaceURI && !div.classList){
-			var createElement = document.createElement;
-			document.createElement = function(name){
-				if("DIV" === (name || "").toUpperCase())
-					return document.createElementNS(NS_HTML, name);
-				return createElement.apply(document, arguments);
+			createElement = DOC[CREATE_ELEMENT];
+			DOC[CREATE_ELEMENT] = function(name){
+				if(DIV === (name || "").toLowerCase())
+					return DOC[CREATE_ELEMENT_NS](NS_HTML, name);
+				return createElement.apply(DOC, arguments);
 			};
 		}
 		
 		
-		var mirror, reflections;
-		Object.defineProperty(SVG.prototype, "classList", {
+		Object.defineProperty(SVG.prototype, CLASS_LIST, {
 			get: function(){
 				var THIS = this;
 				
 				/** This is the first time we're accessing an SVG element's classes */
 				if(!mirror){
-					mirror      = document.createElementNS(NS_HTML, "div");
+					mirror      = DOC[CREATE_ELEMENT_NS](NS_HTML, DIV);
 					reflections = mirror.children;
 					
 					/** Set a listener to respond to changes in the reflection's classes list */
@@ -43,9 +55,9 @@
 						var reflected = target.__R;
 						
 						/** Update the SVG element's class attribute */
-						if("class" === e.attrName && reflected){
+						if(CLASS_ === e.attrName && reflected){
 							reflected[CORKED] = true;
-							reflected.setAttribute("class", target.className);
+							reflected[SET_ATTR](CLASS_, target.className);
 							delete reflected[CORKED];
 						}
 					});
@@ -60,15 +72,15 @@
 				
 				/** No reflection found. Materialise one. */
 				if(!visage){
-					visage = document.createElementNS(NS_HTML, "div");
-					visage.className = THIS.getAttribute("class");
+					visage = DOC[CREATE_ELEMENT_NS](NS_HTML, DIV);
+					visage.className = THIS.getAttribute(CLASS_);
 					mirror.appendChild(visage);
 					visage.__R = THIS;
 					
 					/** Can't be too careful */
-					var setAttr = THIS.setAttribute;
-					THIS.setAttribute = function(name, value){
-						if("class" === name && !THIS[CORKED])
+					var setAttr = THIS[SET_ATTR];
+					THIS[SET_ATTR] = function(name, value){
+						if(CLASS_ === name && !THIS[CORKED])
 							visage.className = value;
 						setAttr.call(THIS, name, value);
 					};
@@ -77,6 +89,5 @@
 				return visage.classList;
 			}
 		});
-		
 	}
 }());
